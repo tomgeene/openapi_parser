252e43405e92316a165c63708130862351e7df24
Tom Geene <tom@talonbytes.com>
2025-11-22 12:53:12 +0100

    empty project

---
+++ lib/openapi_parser.ex
@@ -0,0 +1,18 @@
+defmodule OpenapiParser do
+  @moduledoc """
+  Documentation for `OpenapiParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpenapiParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/openapi_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule OpenapiParserTest do
+  use ExUnit.Case
+  doctest OpenapiParser
+
+  test "greets the world" do
+    assert OpenapiParser.hello() == :world
+  end
+end
